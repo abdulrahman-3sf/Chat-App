01c9431a0a978f5c3e8f4036ca4e3caea8e4805c
Abdulrahman Alassaf. <163548732+abdulrahman-3sf@users.noreply.github.com>
2025-01-29 15:30:30 +0300

    Upload Files

---
+++ client.py
@@ -0,0 +1,257 @@
+import xmlrpc.client
+import uuid
+import time
+
+# Connect to the server
+proxy = xmlrpc.client.ServerProxy("http://127.0.0.1:8080/")
+
+def display_main_menu():
+    print("\nMain Menu:")
+    print("1. Global Chat")
+    print("2. Show Channels")
+    print("3. Exit")
+
+def display_channel_options():
+    print("\nChannel Options:")
+    print("1. Create Channel")
+    print("2. Join Channel")
+    print("3. Leave Channel")
+    print("4. Back to Main Menu")
+
+def display_conversation_menu(is_leader):
+    print("\nConversation Menu:")
+    print("1. Send Message")
+    print("2. Refresh Chat")
+    print("3. Show Number of Users in Channel")
+    print("4. Leave Channel")
+    print("5. Exit Conversation")
+    if is_leader:
+        print("6. Update Channel Name")
+        print("7. Remove User")
+
+
+def login_or_register():
+    while True:
+        print("\nWelcome! Please choose an option:")
+        print("1. Login")
+        print("2. Sign Up")
+        choice = input("Enter choice: ")
+
+        username = input("Enter username: ")
+        password = input("Enter password: ")
+
+        if choice == "1":
+            response = proxy.authenticate_user(username, password)
+            print(response)
+            if response == "Login successful.":
+                return username  # Proceed if login is successful
+        elif choice == "2":
+            response = proxy.register_user(username, password)
+            print(response)
+            if response == "User registered successfully.":
+                return username  # Proceed if registration is successful
+        else:
+            print("Invalid option. Please try again.")
+            
+def show_channels(user_name):
+    print("\nAvailable Channels:")
+    channels = proxy.get_channels()
+    if not channels:
+        print("No available channels.")
+    else:
+        for channel in channels:
+            print(f"- {channel['channel_name']} (ID: {channel['channel_id']})")
+
+    print("\nChannels You Joined:")
+    joined_channels = proxy.get_user_channels(user_name)
+    if not joined_channels:
+        print("You have not joined any channels.")
+    else:
+        for channel in joined_channels:
+            print(f"- {channel['channel_name']} (ID: {channel['channel_id']})")
+
+def fetch_messages(channel_id):
+    messages = proxy.get_messages(channel_id)
+    for message in messages:
+        print(f"[{message['user_name']}] {message['message']}")
+
+def enter_global_chat(user_name):
+    print("\nEntering Global Chat...")
+
+    response = proxy.join_global_chat(user_name)
+    print(response)
+
+    while True:
+        print("\nGlobal Chat Menu:")
+        print("1. Send Message")
+        print("2. Refresh Chat")
+        print("3. Show Number of Users in Chat")
+        print("4. Leave Chat")
+        print("5. Exit Chat")
+
+        choice = input("Select an option: ")
+
+        if choice == "1":
+            message = input("Enter your message: ")
+            response = proxy.send_message_to_global_chat(user_name, message)
+            print(response)
+
+        elif choice == "2":
+            messages = proxy.get_messages_from_global_chat()
+            if messages:
+                print("")
+                for msg in messages:
+                    print(f"[{msg['user_name']}] {msg['message']}")
+            else:
+                print("No messages in the Global Chat.")
+
+        elif choice == "3":
+            num_users = proxy.get_number_of_users_in_global_chat()
+            print(f"There are {num_users} users in the Global Chat.")
+    
+        
+        elif choice == "4":
+            response = proxy.leave_global_chat(user_name)
+            print(response)
+            break
+
+        elif choice == "5":
+            print("Exiting Global Chat...")
+            break
+
+        else:
+            print("Invalid option. Please try again.")
+
+def enter_channel(user_name, channel_id, is_leader):
+    print("\nEntering the channel...")
+    fetch_messages(channel_id)
+
+    while True:
+        display_conversation_menu(is_leader)
+        conversation_choice = input("Select an option: ")
+
+        if conversation_choice == "1":
+            message = input("Enter your Message: ")
+            response = proxy.send_message(user_name, channel_id, message)
+            print(response)
+
+        elif conversation_choice == "2":
+            print("\nRefreshing chat...")
+            fetch_messages(channel_id)
+
+        elif conversation_choice == "3":
+            users = proxy.get_users_in_channel(channel_id)
+            if isinstance(users, list):
+                print(f"There are {len(users)} users in the channel.")
+            else:
+                print("Failed to retrieve users or channel does not exist.")
+    
+        elif conversation_choice == "4":
+            response = proxy.leave_channel(user_name, channel_id)
+            print(response)
+            break
+
+        elif conversation_choice == "5":
+            print("Exiting conversation...")
+            break
+
+        elif conversation_choice == "6" and is_leader:
+            new_name = input("Enter New Channel Name: ")
+            response = proxy.update_channel_name(user_name, channel_id, new_name)
+            print(response)
+
+        elif conversation_choice == "7" and is_leader:
+            users_in_channel = proxy.get_users_in_channel(channel_id)
+
+            if not users_in_channel:
+                print("No users to remove.")
+            else:
+                print("Users in Channel:")
+                for idx, user in enumerate(users_in_channel, start=1):
+                    print(f"{idx}. {user}")
+
+                try:
+                    choice = int(input("Select a user to remove (enter number): "))
+                    if 1 <= choice <= len(users_in_channel):
+                        user_to_remove = users_in_channel[choice - 1]
+                        response = proxy.remove_user_by_leader(user_name, channel_id, user_to_remove)
+                        print(response)
+                    else:
+                        print("Invalid selection.")
+                except ValueError:
+                    print("Invalid input. Please enter a number.")
+
+        else:
+            print("Invalid option. Please try again.")
+
+
+def main():
+    user_name = login_or_register()
+    leader_id = str(uuid.uuid4())
+
+    while True:
+        display_main_menu()
+        choice = input("Select an option: ")
+        
+        if choice == "1":
+            enter_global_chat(user_name)
+        
+        elif choice == "2":
+            show_channels(user_name)
+
+            while True:
+                display_channel_options()
+                option = input("Select an option: ")
+
+                if option == "1":
+                    channel_name = input("Enter Channel Name: ")
+                    response = proxy.create_channel(leader_id, user_name, channel_name)
+                    print(response)
+
+                    if "channel_id" in response:
+                        channel_id = response["channel_id"]
+                        enter_channel(user_name, channel_id, is_leader=True)
+
+                elif option == "2":
+                    channel_name = input("Enter Channel Name: ")
+                    channel_id = proxy.get_channel_id_by_name(channel_name)
+                    
+                    if channel_id is None:
+                        print(f"Channel '{channel_name}' does not exist.")
+                
+                    else:
+                        response = proxy.join_channel(user_name, channel_id)
+                        print(response)
+
+                        if "successfully" in response or "already in" in response:
+                            is_leader = proxy.is_leader(user_name, channel_id)
+                            enter_channel(user_name, channel_id, is_leader)
+
+
+                elif option == "3":
+                    channel_name = input("Enter Channel Name: ")
+                    channel_id = proxy.get_channel_id_by_name(channel_name)
+                    
+                    if channel_id:
+                        response = proxy.leave_channel(user_name, channel_id)
+                        print(response)
+                    else:
+                        print(f"Channel '{channel_name}' does not exist.")
+
+
+                elif option == "4":
+                    print("Returning to Main Menu...")
+                    break
+
+                else:
+                    print("Invalid option. Please try again.")
+
+        elif choice == "3":
+            print("Exiting...")
+            break
+
+        else:
+            print("Invalid option. Please try again.")
+
+if __name__ == "__main__":
+    main()
---
+++ server.py
@@ -0,0 +1,109 @@
+from xmlrpc.server import SimpleXMLRPCServer
+import database
+
+class ChannelServer:
+    
+    # -------------------- User Authentication -------------------- #
+    def register_user(self, username, password):
+        return database.register_user(username, password)
+
+    def authenticate_user(self, username, password):
+        return database.authenticate_user(username, password)
+    
+    
+    # -------------------- Sngle Chat Room -------------------- #
+    def join_global_chat(self, user_name):
+        return database.join_global_chat(user_name)
+
+    def leave_global_chat(self, user_name):
+        return database.leave_global_chat(user_name)
+    
+    def get_number_of_users_in_global_chat(self):
+        return database.get_number_of_users_in_global_chat()
+
+    def send_message_to_global_chat(self, user_name, message):
+        return database.send_message_to_global_chat(user_name, message)
+
+    def get_messages_from_global_chat(self):
+        return database.get_messages_from_global_chat()
+    
+    
+    # -------------------- Channel Chat Rooms -------------------- #
+    def create_channel(self, leader_id, leader_name, channel_name):
+        return database.create_channel(leader_id, leader_name, channel_name)
+
+    def join_channel(self, user_name, channel_id):
+        return database.add_user_to_channel(user_name, channel_id)
+
+    def leave_channel(self, user_name, channel_id):
+        return database.remove_user_from_channel(user_name, channel_id)
+
+    def send_message(self, user_name, channel_id, message):
+        return database.add_message_to_channel(user_name, channel_id, message)
+
+    def get_channels(self):
+        return database.get_all_channels()
+
+    def get_user_channels(self, user_name):
+        return database.get_channels_for_user(user_name)
+
+    def is_leader(self, user_name, channel_id):
+        return database.is_user_leader(user_name, channel_id)
+
+    def get_messages(self, channel_id):
+        return database.get_messages(channel_id)
+    
+    def update_channel_name(self, leader_name, channel_id, new_name):
+        leader_id = None
+        for ch_id, data in database.db["channels"].items():
+            if data["leader_name"] == leader_name:
+                leader_id = data["leader_id"]
+                break
+        return database.update_channel_name(leader_id, channel_id, new_name)
+    
+    def remove_user_by_leader(self, leader_name, channel_id, user_name):
+        return database.remove_user_by_leader(leader_name, channel_id, user_name)
+    
+    def get_users_in_channel(self, channel_id):
+        return database.get_users_in_channel(channel_id)
+
+    def get_channel_id_by_name(self, channel_name):
+        return database.get_channel_id_by_name(channel_name)
+    
+    def get_users_in_channel(self, channel_id):
+        return database.get_users_in_channel(channel_id)
+    
+# Start the server
+server = SimpleXMLRPCServer(("127.0.0.1", 8080), allow_none=True)
+channel_server = ChannelServer()
+
+# Register methods
+
+# Authentication functions
+server.register_function(channel_server.register_user, "register_user")
+server.register_function(channel_server.authenticate_user, "authenticate_user")
+
+# Global chat methods 
+server.register_function(channel_server.join_global_chat, "join_global_chat")
+server.register_function(channel_server.leave_global_chat, "leave_global_chat")
+server.register_function(channel_server.get_number_of_users_in_global_chat, "get_number_of_users_in_global_chat")
+server.register_function(channel_server.send_message_to_global_chat, "send_message_to_global_chat")
+server.register_function(channel_server.get_messages_from_global_chat, "get_messages_from_global_chat")
+
+# Channel chat methods
+server.register_function(channel_server.create_channel, "create_channel")
+server.register_function(channel_server.join_channel, "join_channel")
+server.register_function(channel_server.leave_channel, "leave_channel")
+server.register_function(channel_server.send_message, "send_message")
+server.register_function(channel_server.get_channels, "get_channels")
+server.register_function(channel_server.get_user_channels, "get_user_channels")
+server.register_function(channel_server.is_leader, "is_leader")
+server.register_function(channel_server.get_messages, "get_messages")
+server.register_function(channel_server.update_channel_name, "update_channel_name")
+server.register_function(channel_server.remove_user_by_leader, "remove_user_by_leader")
+server.register_function(channel_server.get_users_in_channel, "get_users_in_channel")
+server.register_function(channel_server.get_channel_id_by_name, "get_channel_id_by_name")
+server.register_function(channel_server.get_users_in_channel, "get_users_in_channel")
+
+print("Server is running on http://127.0.0.1:8080")
+server.serve_forever()
